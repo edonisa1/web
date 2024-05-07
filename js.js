c79e903f3d4df6ba4419d3cc3ef298a1eab11ad7
edonisa1 <edonisabajraktari18@gmail.com>
2024-05-07 18:43:52 +0200

    WEB gr 170

---
+++ js.js
@@ -0,0 +1,19 @@
+var nr1=3;
+var nr2=4;
+var operatori='-';
+switch(operatori){
+    case '+':
+        console.log('Mbledh');
+        break;
+    case '-':
+        console.log('Zbrit');
+        break;
+    case '*':
+        console.log('Shumezim');
+        break;
+    case '/':
+        console.log('Pjestim');
+        break;
+    default:
+        console.log('Operator i panjohur')
+}
